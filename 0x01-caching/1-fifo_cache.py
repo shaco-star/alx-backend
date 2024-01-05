7df39184c037e7a455a0270da0d45a430de6b641
shaco-star <sherifm846@gmail.com>
2024-01-05 12:01:25 -0800

    commit

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """_summary_
+
+    Args:
+        BaseCaching (_type_): _description_
+    """
+    def __init__(self):
+        """_summary_
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+            item (_type_): _description_
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.order.remove(key)
+                self.order.append(key)
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    discarded = self.order.pop(0)
+                    del self.cache_data[discarded]
+                    print(f"DISCARD: {discarded}")
+                self.cache_data[key] = item
+                self.order.append(key)
+
+    def get(self, key):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
